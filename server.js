55824f493793c17f60d603095d76fb1ad5d44b80
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-09-19 14:58:05 -0400

    adding new user registration and error handling css

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ const usersRoutes = require("./routes/users");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/", usersRoutes(db));
-//app.use("/api/widgets", widgetsRoutes(db));
+app.use("/users", usersRoutes(db));
+//app.use("/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
