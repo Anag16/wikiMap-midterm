8754dbef2e0627df8301d78695a86d5b391f2b2f
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-09-19 19:11:17 -0400

    test branch

---
+++ routes/maps.js
@@ -0,0 +1,118 @@
+const express = require("express");
+const { getAllPinsFromDb } = require("../database");
+const { getMapById, getAllMaps, getCoordinates, createNewMap, updateMap, deleteMap } = require("./helpers");
+const router = express.Router();
+
+// how can i use getAllPinsFromDb from the database.js inside router.get
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    // const pinsData = getAllPinsFromDb().then((result) => {
+    //   console.log(result[0]);
+    //   return result[0];
+    // });
+    console.log(req.session);
+    const templateVars = {};
+    if (!req.session) {
+      templateVars.user = null;
+      templateVars.id = null;
+    } else {
+      templateVars.user = req.session.username;
+      templateVars.id = req.session.user_id;
+    }
+    getAllMaps(db)
+      .then(allMaps => {
+        templateVars.allMaps = allMaps;
+        templateVars.mapName = null;
+        console.log(templateVars);
+        res.render('maps_index', templateVars);
+      });
+  });
+
+  // GET /maps/:id to view specific map based on map's id.
+  router.get("/:id", (req, res) => {
+    const mapID = req.params.id;
+    const templateVars = {};
+    if (!mapID) {
+      res.statusCode = 404;
+      if (!req.session.user_id) {
+        templateVars.user = null;
+        templateVars.id = null;
+        templateVars.mapName = null;
+      } else {
+        templateVars.user = req.session.username;
+        templateVars.id = req.session.user_id;
+        templateVars.mapName = null;
+      }
+      res.render('404', templateVars);
+    }
+    const requestedMapId = mapID;
+    getMapById(db, requestedMapId)
+      .then(requestedMap => {
+        templateVars.requestedMap = requestedMap;
+        if (!req.session) {
+          templateVars.user = null;
+          templateVars.id = null;
+        } else {
+          templateVars.user = req.session.username;
+          templateVars.id = req.session.user_id;
+        }
+        templateVars.mapName = requestedMap.title;
+        res.render('maps_show', templateVars);
+      })
+      .catch((err) => {
+        res.statusCode = 404;
+        templateVars.mapName = null;
+        res.render('404', templateVars);
+      });
+  });
+
+  // Add a new map to db only if logged in, then redirect to new map.
+  router.post("/", (req, res) => {
+    const templateVars = {};
+    if (!req.session.user_id) {
+      templateVars.user = null;
+      templateVars.id = null;
+      templateVars.mapName = null;
+      res.statusCode = 401;
+      res.render('401', templateVars);
+    } else {
+      templateVars.user = req.session.username;
+      templateVars.id = req.session.user_id;
+      const values = req.body;
+      const { title, country, city } = values;
+      getCoordinates(country, city)
+        .then(coordinates => {
+          const parsedCoords = JSON.parse(coordinates);
+          const resultsArray = parsedCoords.results[0];
+          const resultsCoords = resultsArray.locations[0];
+          const latitude = resultsCoords.latLng.lat;
+          const longitude = resultsCoords.latLng.lng;
+          const user_id = req.session.user_id;
+          const map = {
+            title,
+            country,
+            city,
+            latitude,
+            longitude,
+            created_at: new Date(),
+            user_id
+          };
+          createNewMap(db, map)
+            .then(newMap => {
+              res.redirect(`/maps/${newMap.id}`);
+            })
+            .catch((err) => {
+              console.log("query error", err.stack);
+              res.statusCode = 400;
+              templateVars.mapName = null;
+              templateVars.message = "Oops, something went wrong.";
+              res.render('400', templateVars);
+            });
+        });
+    }
+  });
+
+
+  return router;
+};
