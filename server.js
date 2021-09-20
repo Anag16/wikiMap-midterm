0619daaf298b835b9ab2052227c1b6ad662ddb29
Ana Ruiz <ruizana393@gmail.com>
2021-09-20 15:19:48 -0400

    working branch

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const usersRoutes = require("./routes/users");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/users", usersRoutes(db));
+
 //app.use("/widgets", widgetsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
