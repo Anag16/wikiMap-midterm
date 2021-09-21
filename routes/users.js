3ab2a9aaa60ae349a20fd368b6d553afe8c64406
Sowjanya Maisagoni <maisagoni@gmail.com>
2021-09-21 16:46:52 -0400

    cleanup

--- routes/users.js
+++ routes/users.js
@@ -59,7 +59,7 @@ module.exports = (db) => {
               console.log('User ID found: ' + existingUser.id);
               req.session.user_id = existingUser.id;
               req.session.email = existingUser.email;
-              req.session.username = existingUser.username; 
+              req.session.username = existingUser.username;
               res.redirect(`/maps/user/${existingUser.id}`);
             } else {
               console.log('User is not registered');
@@ -151,3 +151,4 @@ module.exports = (db) => {
 
   return router;
 };
+
